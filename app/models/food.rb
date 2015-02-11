53cb50686390ee7982989c9e371b994937ae60d9
monolok <antoinebertin@me.com>
2015-02-11 22:17:06 +0100

    remove paperclip from views/models

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,8 +4,8 @@ class Food < ActiveRecord::Base
 	has_many :subs, :through => :joints
 	accepts_nested_attributes_for :subs, reject_if: :all_blank, allow_destroy: true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
