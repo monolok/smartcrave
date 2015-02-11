4b1cd62509c339065ad37ebab3c2c6fa49bb18e4
monolok <antoinebertin@me.com>
2015-02-11 22:10:58 +0100

    removing images table and attributes for food/subs

---
+++ db/migrate/20150211205356_remove_image_from_foods_and_subs.rb
@@ -0,0 +1,14 @@
+class RemoveImageFromFoodsAndSubs < ActiveRecord::Migration
+  def change
+    remove_column :subs, :image_file_name
+    remove_column :foods, :image_file_name
+    remove_column :subs, :image_content_type
+    remove_column :foods, :image_content_type
+    remove_column :subs, :image_file_size
+    remove_column :foods, :image_file_size
+    remove_column :subs, :image_updated_at
+    remove_column :foods, :image_updated_at
+    remove_column :subs, :image_id
+    remove_column :foods, :image_id              
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210000518) do
+ActiveRecord::Schema.define(version: 20150211210840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,9 @@ ActiveRecord::Schema.define(version: 20150210000518) do
   create_table "foods", force: true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "counter"
-    t.integer  "image_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "ideas", force: true do |t|
@@ -34,17 +29,6 @@ ActiveRecord::Schema.define(version: 20150210000518) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "images", force: true do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "filename"
-    t.string   "mime_type"
-    t.integer  "food_id"
-    t.integer  "sub_id"
-    t.binary   "data"
-    t.string   "name"
   end
 
   create_table "joints", force: true do |t|
@@ -57,14 +41,9 @@ ActiveRecord::Schema.define(version: 20150210000518) do
   create_table "subs", force: true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "counter"
-    t.integer  "image_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "users", force: true do |t|
