c26e8fadc588556fc230b0f0a8786e6cc1f7fae3
monolok <antoinebertin@me.com>
2015-03-12 18:32:23 +0100

    Set up signature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,15 @@ function myFace() {
     window.open("https://www.facebook.com/sharer/sharer.php?app_id=209034415775655&sdk=joey&u=https%3A%2F%2Fwww.smartcrave.herokuapp.com/%2F&display=popup&ref=plugin", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=150, left=400, width=500, height=500");
 }
 
+
+$(function () {
+$('[data-toggle="tooltip"]').tooltip()
+})
+})
+function myFace() {
+window.open("https://www.facebook.com/sharer/sharer.php?app_id=209034415775655&sdk=joey&u=https%3A%2F%2Fwww.restool.herokuapp.com/%2F&display=popup&ref=plugin", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=150, left=400, width=500, height=500");
+}
+
 // function myTweet() {
 //     window.open("http://twitter.com/share?text=Here’s an awesome page to make your cravings smart!&url=http://www.smartcrave.org/&hashtags=smartcrave,cravings,health", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=150, left=400, width=500, height=500");
 // }
